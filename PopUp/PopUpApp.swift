ddc88e00f7e93eb423b1887658f2710990c701e5
somaria <daryl.9@gmail.com>
2023-05-27 20:17:08 +0800

    Initial Commit

---
+++ PopUp/PopUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopUpApp.swift
+//  PopUp
+//
+//  Created by daryl on 27/5/23.
+//
+
+import SwiftUI
+
+@main
+struct PopUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
